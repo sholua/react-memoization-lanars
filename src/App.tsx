3165c67dfa0078a51fc9347cba3e681ca046d937
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2023-03-21 12:10:19 +0200

    Console log on re-render App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [appRenderIndex, setAppRenderIndex] = useState(0);
+
+  console.log(`App rendered ${appRenderIndex}`);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setAppRenderIndex((prev) => prev + 1)}>
+        Re-render app
+      </button>
     </div>
   );
 }
