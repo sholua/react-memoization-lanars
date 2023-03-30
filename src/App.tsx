f6a992e5793bae3d13de33aaba0c6c78299226c3
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2023-03-30 11:20:17 +0300

    Modal in parent element

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { useCallback, useMemo, useState } from 'react';
+import Modal from 'react-modal';
 import './App.css';
 import ChildComponent from './components/ChildComponent';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
 
 function App() {
   const [appRenderIndex, setAppRenderIndex] = useState(0);
   const [childText, setChildText] = useState('Child component');
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   console.log(`App rendered ${appRenderIndex}`);
 
@@ -12,16 +27,39 @@ function App() {
 
   const handleClick = useCallback(() => {}, []);
 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="App">
       <button onClick={() => setAppRenderIndex((prev) => prev + 1)}>
         Re-render app
       </button>
+
       <button onClick={() => setChildText((prev) => prev + '!')}>
         Change child text
       </button>
+
       <br />
+
       <ChildComponent params={params} onClick={handleClick} />
+
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+      </Modal>
     </div>
   );
 }
